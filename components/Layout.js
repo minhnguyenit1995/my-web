5d6fd547807865bad55a9f1424867632cd9672db
minhnguyen.it1995 <minhnguyen.it1995@gmail.com>
2020-11-12 13:49:54 +0700

    second commit

---
+++ components/Layout.js
@@ -0,0 +1,22 @@
+import Header from './Header'
+const layoutStyle = {
+  margin : 20 ,
+  padding : 20 ,
+  border : "1px solid #ddd"
+}
+
+const Layout = (props) => (
+  <div style={layoutStyle}>
+    <Header />
+    {props.children}
+    <style jsx global>
+      {`
+          a:hover{
+              text-decoration:underline;
+          }
+      `}
+    </style>
+  </div>
+)
+
+export default Layout
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+import Layout from "../components/layout"
+const About = () => (
+  <Layout>
+    <p>
+      Hello about js
+    </p>
+  </Layout>
+)
+
+export default About
